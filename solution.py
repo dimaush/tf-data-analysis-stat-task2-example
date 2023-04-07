e223f009b98b8446aaeac02b2aeecb365e10cf58
Dmitrii <36790425+dimaush@users.noreply.github.com>
2023-04-08 02:38:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,15 @@ from scipy.stats import norm
 chat_id = 337677338
 
 def solution(p: float, x: np.array) -> tuple:
-    n = x.shape[0]
-    alpha = 1 - p
     s = 0.026
+    
+    n = x.shape[0]
     x -= s
     b0 = (n + 1) / n * np.amax(x)
-    return s + max( b0, 2 * (np.mean(x) - (np.var(x) / n) ** 0.5 * norm.ppf(1 - alpha / 2)) ), \
-           s + max( b0, 2 * (np.mean(x) - (np.var(x) / n) ** 0.5 * norm.ppf(alpha / 2)) )
+    m = np.mean(x)
+    d = (np.var(x) / n) ** 0.5
+    
+    alpha, beta = (1 - p) / 2, (1 - p) / 2
+    z1 = norm.ppf(alpha)
+    z2 = norm.ppf(1 - beta)
+    return s + max(b0, 2 * (m + z1 * d)), s + max(b0, 2 * (m + z2 * d))
