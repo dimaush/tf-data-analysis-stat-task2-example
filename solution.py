cbfddb74313185203b9d3a396c79c658a8959a0c
Dmitrii <36790425+dimaush@users.noreply.github.com>
2023-04-08 05:22:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,14 +6,14 @@ chat_id = 337677338
 
 def solution(p: float, x: np.array) -> tuple:
     s = 0.026
+    x -= s
     
     n = x.shape[0]
-    x -= s
-    b0 = (n + 1) / n * np.amax(x)
     m = np.mean(x)
     d = (np.var(x) / n) ** 0.5
     
     alpha, beta = (1 - p) / 2, (1 - p) / 2
     z1 = norm.ppf(alpha)
     z2 = norm.ppf(1 - beta)
-    return s + max(b0, 2 * (m + z1 * d)), s + max(b0, 2 * (m + z2 * d))
+    
+    return s + max(np.amax(x), 2 * (m + z1 * d)), s + max(np.amax(x), 2 * (m + z2 * d))
