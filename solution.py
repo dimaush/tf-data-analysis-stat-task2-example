629bda82cc95ccb406508eb4f79298827e407f09
Dmitrii <36790425+dimaush@users.noreply.github.com>
2023-04-08 05:39:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,7 @@ chat_id = 337677338
 def solution(p: float, x: np.array) -> tuple:
     s = 0.026
     x -= s
-
-    s = 0.026
-    x -= s
-
+    
     n = x.shape[0]
     x_max = np.amax(x)
-    alpha = (1 - p) ** (1 / n)
-    return (s + x_max, s + x_max / alpha)
+    return s + x_max, s + x_max / ((1 - p) ** (1 / n))
